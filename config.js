83117aa74045e7ca1360edff9441f49a7f432a1b
Paulo Pascua <paulocpascua@gmail.com>
2015-10-19 21:20:28 +0800

    moved to mongolab database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'localhost:27017/aide',
+    'database': 'mongodb://admin:admin@ds039504.mongolab.com:39504/aide',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };

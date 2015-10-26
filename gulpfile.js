09d4b1d11648306eab68f897ecf80ae731a2543c
Paulo Pascua <paulocpascua@gmail.com>
2015-10-26 13:55:46 +0800

    burat

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['nodemon']);
 
-// jshint
+gulp.task('nodemon', function() {
+    nodemon({
+        script: 'server.js',
+        ext: 'js'
+    })
+        .on('start', ['watch'])
+        .on('change', ['watch'])
+        .on('restart', function() {
+            console.log('Restarted!');
+        });
+});
+
 gulp.task('jshint', function() {
-    return gulp.src('public/**/*.js')
+    return gulp.src(['app/**/*.js', 'public/**/*.js', 'server.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('public/**/*.js', ['jshint']);
+    gulp.watch(['app/**/*.js', 'public/**/*.js', 'server.js'] , ['jshint']);
 });
